c6291bb42abd822a2a831ba6ed15473b0e91899b
Dudyala Sumanth -X (dsumanth - INFOSYS LIMITED at Cisco) <dsumanth@partnet.cisco.com>
2019-12-30 11:52:42 +0530

    updated routing for landing page and heatMap page

--- OSDMDashboard/src/app/app-routing.module.ts
+++ OSDMDashboard/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path:'heatmapNav',component:NavMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
